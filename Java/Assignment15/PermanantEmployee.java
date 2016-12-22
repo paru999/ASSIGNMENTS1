7940a9f7b2c61edba33b22767079e50950f6f0b6
paru999 <akhilamohanan000@gmail.com>
2016-12-22 12:44:48 +0530

    assi5

---
+++ Java/Assignment15/PermanantEmployee.java
@@ -0,0 +1,87 @@
+package test;
+
+public class PermanantEmployee  extends employee1  implements promotionalOffers14
+{
+	public  PermanantEmployee(String Name, String Designation, int Age, int PF, int Gratuity, int Salary)
+		{
+		super (Name, Designation, Age, PF, Gratuity, Salary);
+		
+	      }
+	String PermanantCode;
+	public void setpcode(String pcode)
+	{
+		this.PermanantCode=pcode;
+	}
+	public String Getpcode()
+	{
+		return  PermanantCode;
+	}
+	
+	int EMPCode;
+	public void setecode(int ecode)
+	{
+		this.EMPCode=ecode;
+	}
+	public int Getecode()
+	{
+		return  EMPCode;
+	}
+	
+	int PhoneNo;
+	public void setphn(int phn)
+	{
+		this.PhoneNo=phn;
+	}
+	public int Getphn()
+	{
+		return  PhoneNo;
+	}
+	
+	String Gender;
+	public void setgnr(String gnr)
+	{
+		this. Gender=gnr;
+	}
+	public String Getgnr()
+	{
+		return   Gender;
+	}
+	void disp()
+	{
+		//System.out.println("PermanentCode:"+ Getpcode());
+		//System.out.println("EMPCode:"+ Getecode());
+		//System.out.println("PhoneNo:"+ Getphn());
+		//System.out.println("Gender:"+ Getgnr());
+		
+		
+	}
+	@Override
+	public void seasonOffer(int totalPurchaseAmount) 
+	{
+		totalPurchaseAmount=totalPurchaseAmount*10/100;
+		System.out.println("10% of purchase:"+totalPurchaseAmount);
+		
+	}
+	@Override
+	public void regularoffer(int amount)
+	{
+		amount=amount*10/100;
+		System.out.println("25% of purchase:"+amount);
+		
+	}
+	
+	}
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
