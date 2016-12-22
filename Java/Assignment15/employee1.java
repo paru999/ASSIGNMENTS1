46785995d9bf5f5bef468cd0592d98de7815e90f
paru999 <akhilamohanan000@gmail.com>
2016-12-22 12:43:36 +0530

    assi3

---
+++ Java/Assignment15/employee1.java
@@ -0,0 +1,38 @@
+package test;
+
+
+public class employee1
+{
+	String name;
+	String designation;
+	int age,PF,Gratuity,Salary;
+	employee1(String name,String designation,int age,int PF,int Gratuity,int Salary)
+	{
+		this.name=name;
+		this.designation=designation;
+		this.age=age;
+		this.PF=PF;
+		this.Gratuity=Gratuity;
+		this.Salary=Salary;
+		
+	}
+	public int calculateSalary()
+	{
+		return(Salary-(PF+Gratuity));
+	}
+	void empdisplay()
+	 {
+		
+		System.out.println("name:"+name);
+		//System.out.println("designation:"+designation);
+		//System.out.println("age:"+age);
+		
+		//System.out.println("calculateSalary:"+calculateSalary());
+}
+}
+
+
+
+
+	 
+
