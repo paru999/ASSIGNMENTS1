af04de09267de9470f7d6a85ff7b026f0bccc321
paru999 <akhilamohanan000@gmail.com>
2016-12-23 09:53:00 +0530

    assi3

---
+++ Java/Assignment16/customer5.java
@@ -0,0 +1,49 @@
+package test;
+
+public class customer5 implements promotionalOffers14 
+{
+	String Name;
+	String Addres;
+	int Age, PhoneNo;
+		
+	customer5(String name,String Addres, int Age, int PhoneNo)
+	
+		{
+		this.Name=name;
+		this.Addres=Addres;
+		this.Age=Age;
+		this.PhoneNo=PhoneNo;
+		}
+	
+
+	public static void main(String[] args) 
+	{
+		customer5 ass=new customer5("Ammu","Arya Bhavan,Kollam",29,5668877);
+		//System.out.println("Name:"+ass.Name);
+		//System.out.println("Addres:"+ass.Addres);
+		//System.out.println("Age:"+ass.Age);
+		//System.out.println("PhoneNo:"+ass.PhoneNo);
+		System.out.println("          ");
+		ass.seasonOffer(2500);
+		ass.regularoffer(2500);
+	}
+
+
+	
+	public void seasonOffer(int totalPurchaseAmount) 
+	{
+		totalPurchaseAmount=totalPurchaseAmount-totalPurchaseAmount*10/100;
+		System.out.println("10% of purchase:"+totalPurchaseAmount);
+		
+	}
+
+
+	public void regularoffer(int amount) 
+	{
+		amount=amount-amount*0/100;
+		System.out.println("0% of purchase:"+amount);
+	}
+	
+
+
+}
