51c822f7d8a106328774079ec9ff5cf086de3444
paru999 <akhilamohanan000@gmail.com>
2016-12-23 09:59:14 +0530

    assi6

---
+++ Java/Assignment16/cnsmain.java
@@ -0,0 +1,35 @@
+package test;
+
+public class cnsmain  
+{
+	
+	 cnsmain(String name, String designation, int age, int pF, int gratuity, int salary ) 
+	 {
+		
+	}
+
+	public static void main(String[] args)
+	{
+		contractemployee cndr= new contractemployee("Mary", "HR Head",24,150,2500,10000);
+		
+		/*cndr.empdisplay();
+		cndr.setcdur(6);
+		cndr.Getcdur();
+		cndr.setemcd(1002);
+		cndr.Getemcd();
+		cndr.setphno(9823672);
+		cndr.Getphno();
+		cndr.setgnr("Female");
+		cndr.Getgnr();*/
+		cndr.seasonOffer(2500);
+		
+		cndr.regularoffer(2500);
+		
+		cndr.disp();
+		
+
+	}
+
+	
+
+}
