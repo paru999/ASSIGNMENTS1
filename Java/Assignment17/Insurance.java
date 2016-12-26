feaa9703dba4df954132c4f8bd31f68bc716daa7
paru999 <akhilamohanan000@gmail.com>
2016-12-26 12:09:12 +0530

    assi2

---
+++ Java/Assignment17/Insurance.java
@@ -0,0 +1,12 @@
+package test;
+
+public abstract class Insurance 
+    { 
+	int InsuranceNo=5000, InsureAmount=3000, TotalEMI=3000;
+    
+	
+	abstract void registerInsurance();
+	abstract void  payEMI();
+    }
+    
+	
