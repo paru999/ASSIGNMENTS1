06863f76d0d011ecdbfb373297f10d5d0898df98
paru999 <akhilamohanan000@gmail.com>
2016-12-30 12:35:19 +0530

    updated

--- Java/Assignment16/customer5.java
+++ Java/Assignment16/customer5.java
@@ -19,30 +19,76 @@ public class customer5 implements promotionalOffers14
 	public static void main(String[] args) 
 	{
 		customer5 ass=new customer5("Ammu","Arya Bhavan,Kollam",29,5668877);
-		//System.out.println("Name:"+ass.Name);
-		//System.out.println("Addres:"+ass.Addres);
+		System.out.println("Customer Details");
+		System.out.println("                      ");
+		System.out.println("Name:"+ass.Name);
+		System.out.println("Addres:"+ass.Addres);
 		//System.out.println("Age:"+ass.Age);
-		//System.out.println("PhoneNo:"+ass.PhoneNo);
-		System.out.println("          ");
-		ass.seasonOffer(2500);
-		ass.regularoffer(2500);
+		System.out.println("PhoneNo:"+ass.PhoneNo);
+		//System.out.println("          ");
+		
+		
+		int TotalPurchaseAmount=ass.SeasonOffer(2500);
+		int amount=ass.regularOffer(2500);	
+		
+		
+		
+		
+		if(TotalPurchaseAmount>=2500&&amount>=2500)
+		{
+			int A=TotalPurchaseAmount*10/100;
+			
+			int Total=TotalPurchaseAmount-A;
+			System.out.println("TotalPurchaseAmount:"+Total);
+			 int B=amount*0/100;
+			int Totalamount=amount-B;
+			System.out.println("Amount :"+Totalamount);
+			int  payEmi= 0;
+			payEmi=Totalamount-Total;
+			System.out.println("Amount to be paid:"+payEmi);
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	private int SeasonOffer(int TotalPurchaseAmount) {
+		//totalPurchaseAmount=totalPurchaseAmount-totalPurchaseAmount*10/100;
+				//System.out.println("10% of purchase:"+totalPurchaseAmount);
+		// TODO Auto-generated method stub
+		return TotalPurchaseAmount;
+	}
+
+
+	private int regularOffer(int Amount) {
+		//amount=amount-amount*0/100;
+				//System.out.println("0% of purchase:"+amount);
+		
+		return Amount;
+	}
+
+
+	@Override
+	public int seasonOffer(int amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int regularoffer(int amount) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 
 	
-	public void seasonOffer(int totalPurchaseAmount) 
-	{
-		totalPurchaseAmount=totalPurchaseAmount-totalPurchaseAmount*10/100;
-		System.out.println("10% of purchase:"+totalPurchaseAmount);
-		
-	}
 
 
-	public void regularoffer(int amount) 
-	{
-		amount=amount-amount*0/100;
-		System.out.println("0% of purchase:"+amount);
-	}
 	
 
 
