b1734fe237dc3bdf9e00bd49d9fb88f2776904b2
paru999 <akhilamohanan000@gmail.com>
2016-12-30 12:36:51 +0530

    updated

--- Java/Assignment16/permain.java
+++ Java/Assignment16/permain.java
@@ -16,12 +16,27 @@ public class permain
 		
 		//pemp.setpcode("PM100");
 		//pemp.setecode(1001);
-		//pemp.setphn(98967623);
-		//pemp.setgnr("Male");
-		//pemp.disp();
-		pemp.seasonOffer(2500);
-		pemp.regularoffer(2500);
-		pemp.empdisplay();
+		pemp.setphn(98967623);
+		pemp.setgnr("Male");
+		pemp.disp();
+		int TotalPurchaseAmount=pemp.seasonOffer(2500);
+		int amount=pemp.regularoffer(2500);	
+		
+		
+		if(TotalPurchaseAmount>=2500&&amount>=2500)
+		{
+			int A=TotalPurchaseAmount*25/100;
+			
+			int Total=TotalPurchaseAmount-A;
+			System.out.println("TotalPurchaseAmount:"+Total);
+			 int B=amount*10/100;
+			int Totalamount=amount-B;
+			System.out.println("Amount :"+Totalamount);
+			int  payEmi= 0;
+			payEmi=Totalamount-Total;
+			System.out.println("Amount to be paid:"+payEmi);
+		}
+		//pemp.empdisplay();
 		
 		
 
