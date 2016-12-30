81d6c5beb595c3a8b3a05cc4f80ce1cff26e8a83
paru999 <akhilamohanan000@gmail.com>
2016-12-30 12:38:13 +0530

    updated

--- Java/Assignment16/employee1.java
+++ Java/Assignment16/employee1.java
@@ -1,7 +1,7 @@
 package test;
 
 
-public class employee1
+public class employee1  extends insumain  
 {
 	String name;
 	String designation;
@@ -16,12 +16,17 @@ public class employee1
 		this.Salary=Salary;
 		
 	}
-	public int calculateSalary()
+	
+public int calculateSalary()
 	{
 		return(Salary-(PF+Gratuity));
 	}
+
+
 	void empdisplay()
 	 {
+		
+		
 		
 		System.out.println("name:"+name);
 		//System.out.println("designation:"+designation);
@@ -29,7 +34,117 @@ public class employee1
 		
 		//System.out.println("calculateSalary:"+calculateSalary());
 }
-}
+	
+	void registerInsurance() 
+	{
+		String empcode="E100";
+		InsuranceNo="INS_"+getecode()+"670";
+		int InsureAmount=5000;
+	System.out.println("InsuranceNo:"+InsuranceNo);
+		System.out.println("insureAmount:"+GetInsureAmount());
+		
+	}
+
+
+	void payEMI() {
+		System.out.println("EMI:"+getemi());
+		
+	}
+		
+	int claimInsurance() {
+		if(TotalEMI==InsureAmount)
+		{
+		return InsureAmount;
+	   }
+		else {
+			return TotalEMI;
+		}
+
+
+	}
+	public void setInsureAmount(int InsureAmount)
+	{
+		this.InsureAmount=InsureAmount;
+	}
+	public int GetInsureAmount()
+	{
+		return InsureAmount;
+	}
+	String EmpCode;
+	public void setecode(String EmpCode)
+	{
+		this.EmpCode=EmpCode;
+	}
+	private String getecode() {
+		
+		// TODO Auto-generated method stub
+		return  EmpCode;
+	}
+	public void setemi(int totalEMI)
+	{
+		this.TotalEMI=totalEMI;
+		
+	}
+	public int getemi()
+	{
+		return TotalEMI;
+	}
+		
+	public static void main(String[] args) {
+
+		employee1 emp=new employee1("John", "HR", 35, 200, 300, 15000);
+		emp.empdisplay();
+		emp.setecode("B300");
+		emp.setemi(85000);
+		emp.payEMI();
+		emp.setInsureAmount(3500);
+		emp.registerInsurance();
+		if(emp.TotalEMI==emp.InsureAmount)
+		{
+			int insur=emp.claimInsurance();
+			System.out.println(" insureamount:"+insur);
+	   }
+		else {
+			int total=emp.claimInsurance();
+			
+			System.out.println("TotalEMI:"+total);
+		}
+	}
+			
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
