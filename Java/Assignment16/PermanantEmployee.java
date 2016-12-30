31784c132e5384dc97585fd399e2558390184ed6
paru999 <akhilamohanan000@gmail.com>
2016-12-30 12:36:04 +0530

    updated

--- Java/Assignment16/PermanantEmployee.java
+++ Java/Assignment16/PermanantEmployee.java
@@ -46,31 +46,87 @@ public class PermanantEmployee  extends employee1  implements promotionalOffers1
 	{
 		return   Gender;
 	}
-	void disp()
+	
+	
+	public void setInsureAmount(int InsureAmount)
 	{
-		//System.out.println("PermanentCode:"+ Getpcode());
-		//System.out.println("EMPCode:"+ Getecode());
-		//System.out.println("PhoneNo:"+ Getphn());
-		//System.out.println("Gender:"+ Getgnr());
+		this.InsureAmount=InsureAmount;
+	}
+	public int GetInsureAmount()
+	{
+		return InsureAmount;
+	}
+	String EmpCode;
+	public void setecode(String EmpCode)
+	{
+		this.EmpCode=EmpCode;
+	}
+	private String getecode() {
 		
+		// TODO Auto-generated method stub
+		return  EmpCode;
+	}
+	public void setemi(int totalEMI)
+	{
+		this.TotalEMI=totalEMI;
 		
 	}
-	@Override
-	public void seasonOffer(int totalPurchaseAmount) 
+	public int getemi()
 	{
-		totalPurchaseAmount=totalPurchaseAmount-totalPurchaseAmount*25/100;
-		System.out.println("25% of purchase:"+totalPurchaseAmount);
+		return TotalEMI;
+	}
+	
+	void registerInsurance() 
+	{
+		String empcode="E100";
+		InsuranceNo="INS_"+getecode()+"870";
+		int InsureAmount=5000;
+	System.out.println("InsuranceNo:"+InsuranceNo);
+		System.out.println("insureAmount:"+GetInsureAmount());
 		
 	}
-	@Override
-	public void regularoffer(int amount)
+	void PayEmi()
 	{
-		amount=amount-amount*10/100;
-		System.out.println("10% of purchase:"+amount);
+		int emi;
+		emi=GetInsureAmount()*5/100;
+		int PayEMI=emi+getemi();
+		System.out.println("PayEMI:"+PayEMI);
 		
 	}
 	
+	
+	
+	
+	
+	
+	
+	void disp()
+	{
+		System.out.println("PermanentCode:"+ Getpcode());
+		System.out.println("EMPCode:"+ Getecode());
+		System.out.println("PhoneNo:"+ Getphn());
+		System.out.println("Gender:"+ Getgnr());
+		
+		
 	}
+	@Override
+	public int seasonOffer(int totalPurchaseAmount) 
+	{
+		return totalPurchaseAmount;
+		//totalPurchaseAmount=totalPurchaseAmount-totalPurchaseAmount*25/100;
+		//System.out.println("25% of purchase:"+totalPurchaseAmount);
+		
+	}
+	@Override
+	public int regularoffer(int amount)
+	{
+		//amount=amount-amount*10/100;
+		//System.out.println("10% of purchase:"+amount);
+		return amount;
+		
+	}
+	
+}
 
 	
 
