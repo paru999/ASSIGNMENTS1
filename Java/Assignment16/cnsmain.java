868f0d35c9e8efecc6928260eef4cd46d55e0f66
paru999 <akhilamohanan000@gmail.com>
2016-12-30 12:33:53 +0530

    updated

--- Java/Assignment16/cnsmain.java
+++ Java/Assignment16/cnsmain.java
@@ -12,18 +12,33 @@ public class cnsmain
 	{
 		contractemployee cndr= new contractemployee("Mary", "HR Head",24,150,2500,10000);
 		
-		/*cndr.empdisplay();
-		cndr.setcdur(6);
-		cndr.Getcdur();
-		cndr.setemcd(1002);
-		cndr.Getemcd();
-		cndr.setphno(9823672);
-		cndr.Getphno();
-		cndr.setgnr("Female");
-		cndr.Getgnr();*/
-		cndr.seasonOffer(2500);
+		cndr.empdisplay();
+		//cndr.setcdur(6);
+		//cndr.Getcdur();
+		//cndr.setemcd(1002);
+		//cndr.Getemcd();
+		//cndr.setphno(9823672);
+		//cndr.Getphno();
+		//cndr.setgnr("Female");
+		//cndr.Getgnr();
+		int TotalPurchaseAmount=cndr.seasonOffer(2500);
+		int amount=cndr.regularoffer(2500);	
 		
-		cndr.regularoffer(2500);
+		
+		if(TotalPurchaseAmount>=2500&&amount>=2500)
+		{
+			int A=TotalPurchaseAmount*15/100;
+			
+			int Total=TotalPurchaseAmount-A;
+			System.out.println("TotalPurchaseAmount:"+Total);
+			 int B=amount*8/100;
+			int Totalamount=amount-B;
+			System.out.println("Amount :"+Totalamount);
+			int  payEmi= 0;
+			payEmi=Totalamount-Total;
+			System.out.println("Amount to be paid:"+payEmi);
+		}
+		
 		
 		cndr.disp();
 		
